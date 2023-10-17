a1d4c865fcc2340a104078ddd72b228e93ab67a6
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-17 10:40:59 +0700

    Fetch jobs from provided URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import JobInfo from "./JobInfo";
 
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
   const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchJobs = async () => {
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json();
+      setIsLoading(false);
+      setJobs(data);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <div className="loading"></div>
+      </main>
+    );
+  }
 
   return (
     <main>
-      <JobInfo />
+      <JobInfo jobs={jobs} />
     </main>
   );
 };
