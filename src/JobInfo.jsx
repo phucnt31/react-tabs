f48d07a973a6db5d3cc217538e6b4ea802af36ea
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-17 10:52:35 +0700

    Render jobinfo component

--- src/JobInfo.jsx
+++ src/JobInfo.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Duties from "./Duties";
 
 const JobInfo = ({ jobs, currentIndex }) => {
-  const { title, dates, duties, id } = jobs[currentIndex];
+  const { title, company, dates, duties, id } = jobs[currentIndex];
 
-  return <div>JobInfo</div>;
+  return (
+    <section className="jobs-center">
+      <h3>{title}</h3>
+      <p className="job-company">{company}</p>
+      <p className="job-date">{dates}</p>
+      <Duties duties={duties} />
+    </section>
+  );
 };
 
 export default JobInfo;
