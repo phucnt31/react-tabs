6bf300ad2a67bf922f51dc3b3c3a4fb97710e339
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-17 10:56:17 +0700

    Render duties component

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-const Duties = () => {
-  return <div>Duties</div>;
+const Duties = ({ duties }) => {
+  return (
+    <div>
+      {duties.map((duty) => {
+        return (
+          <div className="job-desc" key={uuidv4()}>
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Duties;
